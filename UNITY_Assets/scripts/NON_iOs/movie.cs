84e70958821887feef35e6a59b0342f618b7ba60
OneBillionCat <contactez.bast@gmail.com>
2016-01-10 12:06:29 +0100

    First Commit

---
+++ UNITY_Assets/scripts/NON_iOs/movie.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movie : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop = true;
+		GetComponent<AudioSource>().Play ();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+	}
+}
